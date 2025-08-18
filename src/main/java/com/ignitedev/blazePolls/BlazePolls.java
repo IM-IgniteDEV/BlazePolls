a8e891333c37d9e95b0ac6460d27f1b920c8ae91
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-08-18 14:22:05 +0200

    Integrated ACF and SimpleJsonConfig; added commands, listeners, and scheduled tasks.

--- src/main/java/com/ignitedev/blazePolls/BlazePolls.java
+++ src/main/java/com/ignitedev/blazePolls/BlazePolls.java
@@ -1,17 +1,54 @@
 package com.ignitedev.blazePolls;
 
+import co.aikar.commands.PaperCommandManager;
+import com.twodevsstudio.simplejsonconfig.SimpleJSONConfig;
+import com.twodevsstudio.simplejsonconfig.api.Config;
+import com.ignitedev.blazePolls.command.CreatePollCommand;
+import com.ignitedev.blazePolls.command.PollCommand;
+import com.ignitedev.blazePolls.config.PluginConfig;
+import com.ignitedev.blazePolls.listener.PollListeners;
+import com.ignitedev.blazePolls.manager.CreationManager;
+import com.ignitedev.blazePolls.manager.PollManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BlazePolls extends JavaPlugin {
 
+  private PluginConfig pluginConfig;
+  private PollManager pollManager;
+  private CreationManager creationManager;
+
   @Override
   public void onEnable() {
-    // Plugin startup logic
+    SimpleJSONConfig.INSTANCE.register(this);
 
+    this.pluginConfig = Config.getConfig(PluginConfig.class);
+
+    this.pollManager = new PollManager();
+    this.creationManager = new CreationManager();
+
+    registerListeners();
+    registerCommands();
+    runTasks();
+  }
+
+  private void registerListeners() {
+    Bukkit.getPluginManager()
+        .registerEvents(new PollListeners(this, pluginConfig, pollManager, creationManager), this);
+  }
+
+  private void registerCommands() {
+    PaperCommandManager acf = new PaperCommandManager(this);
+    acf.registerCommand(new CreatePollCommand(pluginConfig, pollManager, creationManager));
+    acf.registerCommand(new PollCommand(pluginConfig, pollManager));
+  }
+
+  private void runTasks() {
+    long interval = Math.max(1, pluginConfig.getCloseCheckIntervalSeconds());
+    Bukkit.getScheduler()
+        .runTaskTimer(this, pollManager::checkExpirations, 20L * interval, 20L * interval);
   }
 
   @Override
-  public void onDisable() {
-    // Plugin shutdown logic
-  }
+  public void onDisable() {}
 }
