b9419d0d7a8bc03f6e41e30cb446803258f4073f
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-08-18 18:57:14 +0200

    Added `VoteClickListener` to handle InventoryClickEvent for voting actions in polls.

---
+++ src/main/java/com/ignitedev/blazePolls/listener/VoteClickListener.java
@@ -0,0 +1,50 @@
+package com.ignitedev.blazePolls.listener;
+
+import com.ignitedev.blazePolls.config.PluginConfig;
+import com.ignitedev.blazePolls.gui.VoteGUI;
+import com.ignitedev.blazePolls.manager.PollManager;
+import com.ignitedev.blazePolls.model.Poll;
+import com.ignitedev.blazePolls.util.TextUtility;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+@RequiredArgsConstructor
+public class VoteClickListener implements Listener {
+  private final PluginConfig config;
+  private final PollManager pollManager;
+
+  @EventHandler
+  public void onClick(InventoryClickEvent event) {
+    if (!(event.getWhoClicked() instanceof Player player)) {
+      return;
+    }
+    Inventory inventory = event.getInventory();
+
+    if (inventory.getHolder() == null) {
+      return;
+    }
+    if (!(inventory.getHolder() instanceof VoteGUI holder)) {
+      return;
+    }
+    event.setCancelled(true);
+    int slot = event.getRawSlot();
+
+    if (slot < 9) {
+      return;
+    }
+    int index = slot - 9;
+    Poll poll = holder.getPoll();
+    if (index < poll.getOptions().size()) {
+      player.sendMessage(
+          TextUtility.parseMiniMessage(
+              pollManager.vote(poll, player, index)
+                  ? config.getMsgVoteRecorded()
+                  : config.getMsgCannotVote()));
+      new VoteGUI(config, pollManager, poll).open(player);
+    }
+  }
+}
