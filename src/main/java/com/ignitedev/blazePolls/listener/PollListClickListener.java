5a8acc7303380d7c6bf317054c8d89c20d32939e
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-08-18 18:58:29 +0200

    Added `PollListClickListener` to handle `InventoryClickEvent` for navigating poll list and opening vote GUI.

---
+++ src/main/java/com/ignitedev/blazePolls/listener/PollListClickListener.java
@@ -0,0 +1,48 @@
+package com.ignitedev.blazePolls.listener;
+
+import com.ignitedev.blazePolls.config.PluginConfig;
+import com.ignitedev.blazePolls.gui.PollListGUI;
+import com.ignitedev.blazePolls.gui.VoteGUI;
+import com.ignitedev.blazePolls.manager.PollManager;
+import com.ignitedev.blazePolls.model.Poll;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class PollListClickListener implements Listener {
+  private final PluginConfig config;
+  private final PollManager pollManager;
+
+  @EventHandler
+  public void onClick(InventoryClickEvent event) {
+    if (!(event.getWhoClicked() instanceof Player)) {
+      return;
+    }
+    Inventory inventory = event.getInventory();
+
+    if (inventory.getHolder() == null) {
+      return;
+    }
+    if (!(inventory.getHolder() instanceof PollListGUI)) {
+      return;
+    }
+    event.setCancelled(true);
+    int slot = event.getRawSlot();
+
+    if (slot < 0) {
+      return;
+    }
+    List<Poll> active = pollManager.getActive();
+
+    if (slot >= active.size()) {
+      return;
+    }
+    new VoteGUI(config, pollManager, active.get(slot)).open((Player) event.getWhoClicked());
+  }
+}
